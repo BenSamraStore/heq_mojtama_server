7dce3ca8cd4c5e173a209d04161ff6c60dd08ada
BenSamraStore <darauemaror@gmail.com>
2025-11-06 17:26:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ function runQuery(q, params = []) {
   return pool.query(q, params);
 }
 
-// ✅ إعداد الرفع والستاتيك
+
 const UPLOADS_DIR = path.join(__dirname, "uploads");
 if (!fs.existsSync(UPLOADS_DIR)) fs.mkdirSync(UPLOADS_DIR, { recursive: true });
 const upload = multer({ dest: UPLOADS_DIR });
@@ -3214,6 +3214,7 @@ app.get("/", (_, res) => {
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
+
 
 
 
